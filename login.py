5aa86539c974478875240ae57f4e8acb81f41413
muks14x <muks14x@gmail.com>
2017-01-10 01:53:22 +0530

    Added prompt for password upon first usage

--- login.py
+++ login.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python
-#Copyright Muks 2016-2020, muks14x@gmail.com
-import os,time,mechanize
-username = 'user.cs16';
-password = 'password';
+#Copyright Mukuntha N S(muks14x) 2016-2020, muks14x@gmail.com
+import os,time,mechanize,pickle
+
+config = '.wifilogin.pickle'
+
+if(not os.path.isfile(config)):
+    username = raw_input('Enter Username:');
+    password = raw_input('Enter Password:');
+    with open(config, 'w') as f:
+        pickle.dump([username,password], f)
+
+with open(config) as f:
+    username, password = pickle.load(f)
+
 def signin():
     br = mechanize.Browser()
     br.set_handle_robots(False)
@@ -12,3 +22,8 @@ def signin():
     br.form['password'] = password
     br.submit()
 signin()
+
+if os.system('ping -c 1 8.8.8.8')==0:
+    print'Successfully logged In!'
+else:
+	print 'Error'
