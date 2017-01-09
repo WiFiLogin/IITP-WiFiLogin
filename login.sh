4a9813251e7e88fb637fa4ea4e1140abefaec17f
Tameesh Biswas <tameeshbiswas1998@gmail.com>
2017-01-10 01:46:47 +0530

    Update login.sh

--- login.sh
+++ login.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 #Copyright 2016 Mukuntha, tameeshb http://tameesh.in/muks
 python login.py;
 ping -q -w 1 -c 1 `ip r | grep default | cut -d ' ' -f 3` > /dev/null && notify-send  'IIT P Wifi' 'Successfully Logged in!' -i /home/tameeshb/terminal.ico || notify-send 'IIT P WiFi' 'Error!' -i /home/tameeshb/terminal.ico;
